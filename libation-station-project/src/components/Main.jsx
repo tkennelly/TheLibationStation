e0f1cfeee1edecb19f8a470927ab8a457d99eeac
jlemenager <jeremy@unitedbyzero.com>
2023-06-28 16:31:10 -0400

    fixed a bunch of errors

--- libation-station-project/src/components/Main.jsx
+++ libation-station-project/src/components/Main.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Home from './Home'
+import DrinkList from './Lists/DrinkList'
+import DrinkPage from './Pages/DrinkPage'
 import NonAlcList from './Lists/NonAlcList'
 import AlcList from './Lists/AlcList'
 import NonAlcPage from './Pages/NonAlcPage'
@@ -7,6 +10,7 @@ import AlcDetails from './Pages/AlcPage'
 
 const Main = () => {
 
+    const [drinks, setDrinks] = useState([])
     const [formState, setFormState] = useState('')
     const handleChange = event => {
         setFormState({ ...formState, [event.target.id]: event.target.value })
@@ -20,17 +24,19 @@ const Main = () => {
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <input type="text" onChange={handleChange} value={formState} />
-                <button type='submit'>Submit</button>
-            </form>
             <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/drinks' element={<DrinkList drinks={drinks} setDrinks={setDrinks} formState={formState} />} />
+            <Route path='/drinks/:strDrink' element={<DrinkPage drinks={drinks} />} />
             <Route path='/nonAlc' element={<NonAlcList/>}/>
             <Route path='/alc' element={<AlcList />} />
             <Route path='/nonAlc/:drinks' element={<NonAlcPage/>}/>
             <Route path='/alc/:id' element={<AlcDetails/>}/>
             </Routes>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={formState} />
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
