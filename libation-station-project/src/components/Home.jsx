e2219e0b0290dd2d1e2eb357843f580c04e103bd
Bryan <b.velez205@gmail.com>
2023-06-28 12:58:11 -0400

    bryan

--- libation-station-project/src/components/Home.jsx
+++ libation-station-project/src/components/Home.jsx
@@ -1,6 +1,17 @@
 const Home = () => {
     return (
+        <div className="home">
         <h3>This is a Landing Page</h3>
+        
+        <img src="/src/assets/Oldfashioned.jpeg" alt="" className="landing-image"/>
+        
+        <p className="welcome">Welcome to The Libation Station! 
+        <br/><br/>
+        Here at The Libation Station, we are passionate about mixology and everything related to the art of crafting delightful libations.
+        <br/><br/>
+        Discover an extensive collection of cocktail recipes, from timeless classics to innovative concoctions that push the boundaries of flavor. Our expertly curated library will awaken your taste buds and empower you to master the art of mixology in the comfort of your own home.</p>
+        
+       </div> 
     )
 }
 
--- libation-station-project/src/components/Nav.jsx
+++ libation-station-project/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <div>
+        <div className='nav'>
             <Link to="/">
                 <h2>Home</h2>
             </Link>
