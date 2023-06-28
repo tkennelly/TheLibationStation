cdd1a7444b6ca46aecec5011bbd5efdb9da4b729
tkennelly <theresa.a.kennelly@gmail.com>
2023-06-28 11:15:06 -0400

    working base files

---
+++ libation-station-project/src/components/Main.jsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
+
+const Main = () => {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} />
+        </Routes>
+    )
+}
+
+export default Main
---
+++ libation-station-project/src/components/Nav.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom'
+
+
+const Nav = () => {
+    return (
+        <div>
+            <Link to="/">
+                <h2>Home</h2>
+            </Link>
+        </div>
+    )
+}
+
+export default Nav
