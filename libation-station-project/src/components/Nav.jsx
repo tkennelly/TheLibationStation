e96cd61c63a69263826452e34fb6a98065998a6e
jlemenager <jeremy@unitedbyzero.com>
2023-06-28 14:10:21 -0400

    added more styling to header

--- libation-station-project/src/components/Nav.jsx
+++ libation-station-project/src/components/Nav.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
     return (
         <div className='nav'>
+            <input type="text" value="" placeholder='Search for your favorite cock'/>
             <Link className='link' to="/">Home</Link>
             <Link className='link' to='/drinks'>All Drinks</Link>
         </div>
