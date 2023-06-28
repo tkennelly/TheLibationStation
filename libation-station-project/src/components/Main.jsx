3caf230b2f0439c82cf7d9934cd6e05fb5e936bb
jlemenager <jeremy@unitedbyzero.com>
2023-06-28 16:12:23 -0400

    added stuff to main

--- libation-station-project/src/components/Main.jsx
+++ libation-station-project/src/components/Main.jsx
@@ -6,15 +6,32 @@ import NonAlcPage from './Pages/NonAlcPage'
 import AlcDetails from './Pages/AlcPage'
 
 const Main = () => {
+
+    const [formState, setFormState] = useState('')
+    const handleChange = event => {
+        setFormState({ ...formState, [event.target.id]: event.target.value })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+
+        setFormState('')
+    }
+
     return (
-        <Routes>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={formState} />
+                <button type='submit'>Submit</button>
+            </form>
+            <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/nonAlc' element={<NonAlcList/>}/>
             <Route path='/alc' element={<AlcList />} />
             <Route path='/nonAlc:drinks' element={<NonAlcPage/>}/>
             <Route path='/alc:id' element={<AlcDetails/>}/>
-        
-        </Routes>
+            </Routes>
+        </div>
     )
 }
 
--- libation-station-project/src/components/Nav.jsx
+++ libation-station-project/src/components/Nav.jsx
@@ -1,11 +1,8 @@
 import { Link } from 'react-router-dom'
 
-export let input = <input type="text" placeholder='Search for your favorite cock'/>
-
 const Nav = () => {
     return (
         <div className='nav'>
-            {input}
             <Link className='link' to="/">Home</Link>
             <Link className='link' to='/drinks'>All Drinks</Link>
             <Link className='link' to='/alc'>Alcoholic Drinks</Link>
