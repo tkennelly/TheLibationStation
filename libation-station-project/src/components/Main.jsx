78c0b9475e4feb3da3190b2bced429c159cfafbb
tkennelly <theresa.a.kennelly@gmail.com>
2023-06-28 15:46:21 -0400

    routes

--- libation-station-project/src/components/Main.jsx
+++ libation-station-project/src/components/Main.jsx
@@ -1,14 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import NonAlcList from './Lists/NonAlcList'
+import AlcList from './Lists/AlcList'
+import NonAlcPage from './Pages/NonAlcPage'
+import AlcDetails from './Page/AlcPage'
 
 const Main = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/nonAlc' element={<NonAlcList/>}/>
-            
-            
+            <Route path='/alc' element={<AlcList />} />
+            <Route path='/nonAlc:drinks' element={<NonAlcPage/>}/>
+            <Route path='/alc:id' element={<AlcDetails/>}/>
         
         </Routes>
     )
