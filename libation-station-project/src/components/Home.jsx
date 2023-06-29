8f359107cd5ac0b8a3c5d242ab02ca6e9277531a
Bryan <b.velez205@gmail.com>
2023-06-28 20:10:28 -0400

    bryan

--- libation-station-project/src/components/Home.jsx
+++ libation-station-project/src/components/Home.jsx
@@ -1,15 +1,17 @@
+import oldFashioned from '../assets/Oldfashioned.jpeg'
+
 const Home = () => {
     return (
         <div className="home">
         
-        <img src=".../assets/Oldfashioned.jpeg" alt="" className="landing-image"/>
+        
         
         <p className="welcome">Welcome to The Libation Station! 
         <br/><br/>
         Here at The Libation Station, we are passionate about mixology and everything related to the art of crafting delightful libations.
         <br/><br/>
         Discover an extensive collection of cocktail recipes, from timeless classics to innovative concoctions that push the boundaries of flavor. Our expertly curated library will awaken your taste buds and empower you to master the art of mixology in the comfort of your own home.</p>
-        
+        <img src= {oldFashioned} alt="" className="landing-image"/>
        </div> 
     )
 }
